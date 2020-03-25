200c51a0a16b5a839db8de5044b62c0614a27493
Samuel Gaborieau <sam.gabor@hotmail.com>
2020-03-25 07:41:18 +0100

    add top level menu page with custom icon

--- lorem-plugin.php
+++ lorem-plugin.php
@@ -10,3 +10,23 @@
  * License URI: https://www.mozilla.org/en-US/MPL/2.0/
  * Requires PHP: 7.0.0
  */
+
+// fx prefix: "loremPlugin_"
+
+// add top level menu
+const LOREM_PLUGIN_MENU_POSITION = 5 ;
+
+function loremPlugin_addMenu() {
+
+    add_menu_page(
+        "Lorem Plugin" ,
+        "Lorem Plugin" ,
+        "manage_options" ,
+        "lorem-plugin" ,
+        "" , // callback name or anon fx
+        "dashicons-text" , // icon URL , from library: https://developer.wordpress.org/resource/dashicons/#text
+        LOREM_PLUGIN_MENU_POSITION
+    ) ;
+}
+
+add_action('admin_menu' , 'loremPlugin_addMenu' ) ;
