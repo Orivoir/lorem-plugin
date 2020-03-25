77e6383fab2f736951ee2a592bfd521858279d41
Samuel Gaborieau <sam.gabor@hotmail.com>
2020-03-25 07:58:31 +0100

    add callback show factory data and fix default value factory

--- lorem-plugin.php
+++ lorem-plugin.php
@@ -16,10 +16,10 @@
 // define default value from config
 
 // should be an string with min lengt: 3 and max length: 255 should normalize before check with trim function
-$textIpsum = 'Lorem ipsum dolor sit amet, consectum couram victorma.' ;
+const TEXT_IPSUM_DEFAULT = 'Lorem ipsum dolor sit amet, consectum couram victorma.' ;
 
-// should be NULL or contains an valid email format
-$email = NULL ;
+// should be NULL or contains an valid email format ( stock inside db if valid and not NULL )
+const EMAIL_IPSUM_DEFAULT = NULL ;
 
 // add top level menu
 const LOREM_PLUGIN_MENU_POSITION = 5 ;
@@ -31,7 +31,7 @@ function loremPlugin_addMenu() {
         "Lorem Plugin" ,
         "manage_options" , // roles user for read data
         "lorem-plugin" , // slug content page
-        "" , // callback name or anon fx , show factory data
+        "loremPlugin_showFactoryData" , // callback name: show factory data
         "dashicons-text" , // icon URL , from library: https://developer.wordpress.org/resource/dashicons/#text
         LOREM_PLUGIN_MENU_POSITION
     ) ;
@@ -48,3 +48,26 @@ function loremPlugin_addMenu() {
 }
 
 add_action('admin_menu' , 'loremPlugin_addMenu' ) ;
+
+// callback show factory data
+function loremPlugin_showFactoryData() {
+
+    // here get option
+
+    ?>
+        <h1>
+            Factory data page
+        </h1>
+
+        <div>
+            <p>
+                Ipsum text: <strong><?= TEXT_IPSUM_DEFAULT ?></strong>
+            </p>
+
+            <p>
+                Ipsum email: <strong><?= EMAIL_IPSUM_DEFAULT ?? "email undefined" ?></strong>
+            </p>
+        </div>
+    <?php
+
+}
